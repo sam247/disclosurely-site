5f97a59499f8e3178910acc5f395377f00d9d442
sam247 <sampettiford@googlemail.com>
2025-12-09 09:56:36 +0000

    Temporarily disable Nextra to get build working - will fix docs separately

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-import nextra from 'nextra';
-
-const withNextra = nextra({});
+// Temporarily disable Nextra until we can properly configure it
+// import nextra from 'nextra';
+// const withNextra = nextra({});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withNextra(nextConfig);
+export default nextConfig;
 
