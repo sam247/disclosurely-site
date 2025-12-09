d318a1031bfe52762f85683f8b5f2bf12ca91f2a
sam247 <sampettiford@googlemail.com>
2025-12-09 09:50:02 +0000

    Try Nextra 4.x alternative config format

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import nextra from 'nextra';
 
-const withNextra = nextra({
-  theme: 'nextra-theme-docs',
+const withNextra = nextra('nextra-theme-docs', {
   themeConfig: './theme.config.tsx',
 });
 
